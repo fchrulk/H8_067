728643d53e56d9a336943699f87460a7745973f4
fchrulk <fachrul.kurniansyah@gmail.com>
2021-12-22 23:53:11 +0800

    add example model deployment

---
+++ H8DeployModel/app.py
@@ -0,0 +1,35 @@
+import flask
+import numpy as np
+import pickle
+
+model = pickle.load(open('model/model_classifier.pkl', 'rb'))
+
+app = flask.Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def main():
+    return(flask.render_template('main.html'))
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in flask.request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = {0: 'not placed', 1: 'placed'}
+
+    return flask.render_template('predicted.html', 
+                                 prediction_text=output[prediction[0]],
+                                 prediction_text_title='Prediction Result', 
+                                 prediction_text_1='Student must be ',
+                                 prediction_text_2=' to workplace',)
+
+@app.route('/pandas_profiling')
+def pandas_prof():
+    return(flask.render_template('pandas_profiling.html'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
